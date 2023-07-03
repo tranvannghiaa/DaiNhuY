cf0b7837a99c0688f36354a47034d0e5453fe618
ADMIN <ADMIN@DESKTOP-2TDD628>
2023-07-03 20:28:23 +0700

    Add project files.

---
+++ DaiNhuY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DaiNhuY.Startup))]
+namespace DaiNhuY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
